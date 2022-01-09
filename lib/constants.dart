6e9b110bb84f46aee7a98089bfbdab2422e714fa
nutcpl <59905941+nutcpl@users.noreply.github.com>
2022-01-09 16:34:32 +0700

    add all colors (#1)

---
+++ lib/constants.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+// const primaryColor = Color(0xFF6F35A5);
+const bgColor = Color(0xFFFFFFFF);
+
+// core color
+
+//purple
+const primaryLightest = Color(0xFFE6E3FF);
+const primaryLighter  = Color(0xFFCDC7FF);
+const primaryLight    = Color(0xFFA89EFF);
+const primaryColor    = Color(0xFF8374FF);
+const primaryDark     = Color(0xFF6A5ECE);
+const primaryDarker   = Color(0xFF564CA7);
+
+//pink
+const secoundaryLightest = Color(0xFFFFEAEF);
+const secoundaryLighter  = Color(0xFFFFD6E0);
+const secoundaryLight    = Color(0xFFFFB7C9);
+const secoundary         = Color(0xFFFF98B2);
+const secoundaryDark     = Color(0xFFCF7B90);
+const secoundaryDarker   = Color(0xFFA76475);
+
+//yellow
+const tertiaryLightest = Color(0xFFFFF4E1);
+const tertiaryLighter  = Color(0xFFFFEAC4);
+const tertiaryLight    = Color(0xFFFFDA98);
+const tertiary         = Color(0xFFFFCA6C);
+const tertiaryDark     = Color(0xFFCFA457);
+const tertiaryDarker   = Color(0xFFA78547);
+
+//typography color
+const textDark      = Color(0xFF222E3C);
+const textLight     = Color(0xFFFFFFFF);
+const textMadatory  = Color(0xFFF54E6A);
+
+//grey scale color
+const greyLightest  = Color(0xFFF7F7F7);
+const greyLighter   = Color(0xFFEDEDED);
+const greyLight     = Color(0xFFE2E2E2);
+const grey          = Color(0xFFCBCBCB);
+const greyDark      = Color(0xFF909090);
+const greyDarker    = Color(0xFF6A6A6A);
+
+const negativeLighter   = Color(0xFFFFEEE8);
+const negativeLight     = Color(0xFFFFE6DE);
+const negative          = Color(0xFFFFA688);
+const negativeDark      = Color(0xFFFD9775);
+const negativeDarker    = Color(0xFFE38869);
