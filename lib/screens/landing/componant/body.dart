c04b554d8c05b4cc4a7421a406e8f2dbfb63b6ab
nutcpl <59905941+nutcpl@users.noreply.github.com>
2022-01-11 14:27:26 +0700

    styling landing page

--- lib/screens/landing/componant/body.dart
+++ lib/screens/landing/componant/body.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/componant/account_exists_login.dart';
 import 'package:flutter_application_1/componant/background.dart';
 import 'package:flutter_application_1/componant/round_button.dart';
 import 'package:flutter_application_1/componant/term_and_policies.dart';
 import 'package:flutter_application_1/componant/text_button.dart';
 import 'package:flutter_application_1/constants.dart';
+import 'package:flutter_svg/svg.dart';
 
 class Body extends StatelessWidget {
   const Body({ Key? key }) : super(key: key);
@@ -15,23 +17,26 @@ class Body extends StatelessWidget {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
+          SizedBox(height: size.height * 0.1,),
+          SvgPicture.asset(
+            'assets/icons/landing_logo.svg',
+            height: size.height * 0.2,),
           // ignore: prefer_const_constructors
+          SizedBox(height: size.height * 0.05,),
           Text(
             "มาแลกเปลี่ยนความรู้กันเถอะ !",
             style: TextStyle(
               fontSize: 18, 
               fontWeight: FontWeight.w500,
-              color: grey),
+              color: greyDark),
           ),
-          // SizedBox(height: size.height * 0.5,),
+          SizedBox(height: size.height * 0.2,),
           RoundButton(
             text: 'ลงทะเบียนสำหรับผู้ใช้ใหม่',
             minimumSize: Size(279, 36),
             press: () => {},),
-          TextLinkButton(
-            text: 'มีบัญชีผู้ใช้อยู่แล้ว',
-            press: () => {},
-          ),
+          AccountExistsLogin(),
+          SizedBox(height: size.height * 0.05,),
           TermAndPolicies()
         ],
       ),
