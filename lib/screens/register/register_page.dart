70c1b86156c6e31c8f64752ac2c0c12c0d9814c0
nutcpl <59905941+nutcpl@users.noreply.github.com>
2022-01-12 16:06:13 +0700

    add register page

---
+++ lib/screens/register/register_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/screens/register/component/body.dart';
+
+class RegisterPage extends StatelessWidget {
+  const RegisterPage({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Body(),);
+  }
+}
