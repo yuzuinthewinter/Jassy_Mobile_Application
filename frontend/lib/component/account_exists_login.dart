0f20730ad599403fc17198fa0c17dc42ee859a08
nutcpl <59905941+nutcpl@users.noreply.github.com>
2022-02-23 00:18:07 +0700

    add navigator push

--- frontend/lib/component/account_exists_login.dart
+++ frontend/lib/component/account_exists_login.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/screens/login/login.dart';
 import 'package:flutter_application_1/theme/index.dart';
 
 class AccountExistsLogin extends StatelessWidget {
@@ -9,23 +10,29 @@ class AccountExistsLogin extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return RichText(
-      textAlign: TextAlign.center,
-      text: TextSpan(
-      style: TextStyle(
-        color: greyDark,
-        fontFamily: 'Kanit',
-        ),
-      children: [
-        TextSpan(text: 'มีบัญชีผู้ใช้อยู่แล้ว ? '),
-        TextSpan(
-          text: 'เข้าสู่ระบบ ',
-          style: TextStyle(color: primaryColor,),
-          recognizer: TapGestureRecognizer()
-            ..onTap = () => {
-              print('เข้าสู่ระบบ')
-            }),
-      ]
-    ),);
+    return InkWell(
+      child: RichText(
+        textAlign: TextAlign.center,
+        text: TextSpan(
+        style: TextStyle(
+          color: greyDark,
+          fontFamily: 'Kanit',
+          ),
+        children: [
+          TextSpan(text: 'มีบัญชีผู้ใช้อยู่แล้ว ? '),
+          TextSpan(
+            text: 'เข้าสู่ระบบ ',
+            style: TextStyle(color: primaryColor,),
+            recognizer: TapGestureRecognizer()
+              ..onTap = () => {
+                Navigator.push(
+                  context, 
+                  MaterialPageRoute(builder: (context) => const LoginPage())
+                ),
+                print('เข้าสู่ระบบ')
+              }),
+        ]
+      ),),
+    );
   }
 }
