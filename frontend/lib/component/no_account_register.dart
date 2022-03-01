54b2b661ff9ebf544b67c0ce25cfe21f215566a1
nutcpl <59905941+nutcpl@users.noreply.github.com>
2022-03-01 10:06:13 +0700

    no account then register

---
+++ frontend/lib/component/no_account_register.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/screens/register/register_page.dart';
+import 'package:flutter_application_1/theme/index.dart';
+
+class NoAccountRegister extends StatelessWidget {
+  const NoAccountRegister({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      child: RichText(
+        textAlign: TextAlign.center,
+        text: TextSpan(
+        style: TextStyle(
+          color: greyDark,
+          fontFamily: 'Kanit',
+          ),
+        children: [
+          TextSpan(text: 'ยังไม่มีบัญชีผู้ใช้ ? '),
+          TextSpan(
+            text: 'ลงทะเบียนสำหรับผู้ใช้ใหม่ ',
+            style: TextStyle(color: primaryColor,),
+            recognizer: TapGestureRecognizer()
+              ..onTap = () => {
+                Navigator.push(
+                  context, 
+                  MaterialPageRoute(builder: (context) => const RegisterPage())
+                ),
+                print('ลงทะเบียนสำหรับผู้ใช้ใหม่')
+              }),
+        ]
+      ),),
+    );
+  }
+}
