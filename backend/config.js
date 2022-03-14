3117adf1601551cdb9e51d1c2f10da406172b290
dnazzx <zzan-official@gmail.com>
2022-03-14 18:56:53 +0700

    Remove unnecessary folder and file

--- backend/config.js
+++
@@ -1,9 +0,0 @@
-exports.PORT = process.env.PORT || 5000;
-exports.MONGODB_URI = process.env.MONGODB_URI;
-exports.NODE_ENV = process.env.NODE_ENV;
-
-exports.JWT_SECRET = process.env.JWT_SECRET;
-exports.ORIGIN = process.env.ORIGIN;
-
-exports.FAST2SMS = process.env.FAST2SMS;
-exports.ADMIN_PHONE = process.env.ADMIN_PHONE;
