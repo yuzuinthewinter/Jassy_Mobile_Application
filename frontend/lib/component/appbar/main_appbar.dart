cbe4a3a95f0e8a1fb1d085f769c84ff2fba41670
nutcpl <59905941+nutcpl@users.noreply.github.com>
2022-04-10 13:13:55 +0700

    add main page appbar

--- frontend/lib/component/appbar/main_appbar.dart
+++ frontend/lib/component/appbar/main_appbar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/screens/main/filter.dart';
 import 'package:flutter_application_1/theme/index.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
@@ -18,14 +19,11 @@ class JassyMainAppBar extends StatelessWidget implements PreferredSizeWidget {
       centerTitle: true,
       elevation: 0,
       backgroundColor: Colors.transparent,
-      // leading: new IconButton(
-      //   icon: Icon(Icons.arrow_back_ios, size: 20,),
-      //   color: primaryDarker,
-      //   onPressed: () => Navigator.of(context).pop(),
-      // ),
       actions: [
         IconButton(
-          onPressed: () {}, 
+          onPressed: () {
+            Navigator.push(context, MaterialPageRoute(builder: (context) => const Filter()));
+          }, 
           icon: SvgPicture.asset('assets/icons/filtering.svg',), 
           color: primaryDarker,)
       ],
