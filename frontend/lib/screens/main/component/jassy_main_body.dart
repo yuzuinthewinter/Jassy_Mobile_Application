85261b309484e5f5e6d5b8602a61e0c12f609a7e
nutcpl <59905941+nutcpl@users.noreply.github.com>
2022-04-10 13:15:09 +0700

    main page decoration

--- frontend/lib/screens/main/component/jassy_main_body.dart
+++ frontend/lib/screens/main/component/jassy_main_body.dart
@@ -1,8 +1,10 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/component/curved_widget.dart';
 import 'package:flutter_application_1/component/header_style/jassy_gradient_color.dart';
 import 'package:flutter_application_1/models/user.dart';
 import 'package:flutter_application_1/theme/index.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class JassyMainBody extends StatefulWidget {
   const JassyMainBody({ Key? key }) : super(key: key);
@@ -32,25 +34,35 @@ class _JassyMainBodyState extends State<JassyMainBody> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Column(
-        children: [
-          const CurvedWidget(
-            child: JassyGradientColor()
-          ),
-          AspectRatio(
+    return Column(
+      children: [
+        const CurvedWidget(
+          child: JassyGradientColor()
+        ),
+        CarouselSlider.builder(
+          itemCount: dataLists.length, 
+          itemBuilder: (context, index, child) {
+            return carouselView(index);
+          }, 
+          options: CarouselOptions(
+            // height: size.height * 0.70,
             aspectRatio: 0.75,
-            child: PageView.builder(
-              itemCount: dataLists.length,
-              physics: const ClampingScrollPhysics(),
-              controller: _pageController,
-              itemBuilder: (context, index) {
-                return carouselView(index);
-              }
-            ),
+            enlargeCenterPage: true,
+            enlargeStrategy: CenterPageEnlargeStrategy.height,
           )
-        ],
-      ),
+        ),
+        // AspectRatio(
+        //   aspectRatio: 0.75,
+        //   child: PageView.builder(
+        //     itemCount: dataLists.length,
+        //     physics: const ClampingScrollPhysics(),
+        //     controller: _pageController,
+        //     itemBuilder: (context, index) {
+        //       return carouselView(index);
+        //     }
+        //   ),
+        // )
+      ],
     );
   }
 
@@ -74,25 +86,48 @@ class _JassyMainBodyState extends State<JassyMainBody> {
               )
             ),
           ),
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Align(alignment: Alignment.topRight, child: SvgPicture.asset("assets/icons/close_circle.svg")),
+          ),
           Column(
             mainAxisAlignment: MainAxisAlignment.end,
             children: [
               Container(
-                height: 110,
+                height: size.height * 0.15,
                 width: double.infinity,
                 decoration: BoxDecoration(
                   color: Colors.black.withOpacity(0.5),
                   borderRadius: const BorderRadius.vertical(bottom: Radius.circular(20))
                 ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+                child: Row(
+                  // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
-                    RichText(
-                      text: TextSpan(
-                        style: const TextStyle(fontSize: 24, fontFamily: "kanit"),
-                        children: [TextSpan(text: data.name), const TextSpan(text: ", "), TextSpan(text: data.age)]
-                      ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      // mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        RichText(
+                          text: TextSpan(
+                            style: const TextStyle(fontSize: 12, fontFamily: "kanit", fontWeight: FontWeight.w700),
+                            children: [TextSpan(text: data.country), const TextSpan(text: ", "), TextSpan(text: data.age)]
+                          ),
+                        ),
+                        RichText(
+                          text: TextSpan(
+                            style: const TextStyle(fontSize: 24, fontFamily: "kanit", fontWeight: FontWeight.w900),
+                            children: [TextSpan(text: data.name), const TextSpan(text: ", "), TextSpan(text: data.age)]
+                          ),
+                        ),
+                        RichText(
+                          text: const TextSpan(
+                            style: TextStyle(fontSize: 14, fontFamily: "kanit", fontWeight: FontWeight.w700),
+                            children: [TextSpan(text: "TH"), WidgetSpan(child: Icon(Icons.sync_alt, size: 20, color: textLight,)), TextSpan(text: "KR"),]
+                          ),
+                        ),
+                      ],
                     ),
+                    Expanded(child: SvgPicture.asset("assets/icons/heart_button.svg"))
                   ],
                 ),
               ),
