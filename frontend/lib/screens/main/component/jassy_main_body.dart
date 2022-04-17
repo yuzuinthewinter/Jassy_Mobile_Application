8b42ca3ced0d1e388ab143981a2bffb731ca62f0
nutcpl <59905941+nutcpl@users.noreply.github.com>
2022-04-17 16:16:52 +0700

    main style

--- frontend/lib/screens/main/component/jassy_main_body.dart
+++ frontend/lib/screens/main/component/jassy_main_body.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application_1/component/curved_widget.dart';
 import 'package:flutter_application_1/component/header_style/jassy_gradient_color.dart';
 import 'package:flutter_application_1/models/user.dart';
+import 'package:flutter_application_1/screens/main/component/detail_page.dart';
 import 'package:flutter_application_1/theme/index.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
@@ -51,17 +52,6 @@ class _JassyMainBodyState extends State<JassyMainBody> {
             enlargeStrategy: CenterPageEnlargeStrategy.height,
           )
         ),
-        // AspectRatio(
-        //   aspectRatio: 0.75,
-        //   child: PageView.builder(
-        //     itemCount: dataLists.length,
-        //     physics: const ClampingScrollPhysics(),
-        //     controller: _pageController,
-        //     itemBuilder: (context, index) {
-        //       return carouselView(index);
-        //     }
-        //   ),
-        // )
       ],
     );
   }
@@ -72,83 +62,105 @@ class _JassyMainBodyState extends State<JassyMainBody> {
 
   Widget carouselCard(MainUser data) {
     var size = MediaQuery.of(context).size;
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Stack(
-        children: [
-          Container(
-            width: size.width,
-            height: size.height,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(20),
-              image: DecorationImage(
-                image: AssetImage(data.image), fit: BoxFit.cover
-              )
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: InkWell(
-              onTap: () { print("x"); },
-              child: Align(
-                alignment: Alignment.topRight, 
-                child: SvgPicture.asset("assets/icons/close_circle.svg")
-              )
-            ),
-          ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              Container(
-                height: size.height * 0.15,
-                width: double.infinity,
+    return GestureDetector(
+      onTap: (() {
+        Navigator.of(context).push(
+          PageRouteBuilder(
+            pageBuilder: (context, animation, secondaryAnimation) {
+              final curvedAnimation = CurvedAnimation(
+                parent: animation,
+                curve: Interval(0, 0.5),
+              );
+              return FadeTransition(
+                opacity: curvedAnimation,
+                child: DetailPage(user: data, animation: animation),
+              );
+            }
+          )
+        );
+        print("each card");
+      }),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Stack(
+          children: [
+            Hero(
+              tag: data.image,
+              child: Container(
+                width: size.width,
+                height: size.height,
                 decoration: BoxDecoration(
-                  color: Colors.black.withOpacity(0.5),
-                  borderRadius: const BorderRadius.vertical(bottom: Radius.circular(20))
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(left: 20),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          RichText(
-                            text: TextSpan(
-                              style: const TextStyle(fontSize: 12, fontFamily: "kanit", fontWeight: FontWeight.w700),
-                              children: [TextSpan(text: data.country), const TextSpan(text: ", "), TextSpan(text: data.city), const TextSpan(text: " "), TextSpan(text: data.time)]
-                            ),
-                          ),
-                          RichText(
-                            text: TextSpan(
-                              style: const TextStyle(fontSize: 24, fontFamily: "kanit", fontWeight: FontWeight.w900),
-                              children: [TextSpan(text: data.name), const TextSpan(text: ", "), TextSpan(text: data.age)]
-                            ),
-                          ),
-                          RichText(
-                            text: const TextSpan(
-                              style: TextStyle(fontSize: 14, fontFamily: "kanit", fontWeight: FontWeight.w700),
-                              children: [TextSpan(text: "TH"), WidgetSpan(child: Icon(Icons.sync_alt, size: 20, color: textLight,)), TextSpan(text: "KR"),]
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(right: 20),
-                      child: InkWell(
-                        onTap: () { print("หัวใจ"); },
-                        child: Expanded(child: SvgPicture.asset("assets/icons/heart_button.svg"))
-                      ),
-                    )
-                  ],
+                  borderRadius: BorderRadius.circular(20),
+                  image: DecorationImage(
+                    image: AssetImage(data.image), fit: BoxFit.cover
+                  )
                 ),
               ),
-            ],
-          )
-        ],
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Align(
+                alignment: Alignment.topRight, 
+                child: InkWell(
+                  onTap: () { print("x"); },
+                  child: SvgPicture.asset("assets/icons/close_circle.svg")
+                )
+              )
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Container(
+                  height: size.height * 0.15,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: Colors.black.withOpacity(0.5),
+                    borderRadius: const BorderRadius.vertical(bottom: Radius.circular(20))
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(left: 20),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            RichText(
+                              text: TextSpan(
+                                style: const TextStyle(fontSize: 12, fontFamily: "kanit", fontWeight: FontWeight.w700),
+                                children: [TextSpan(text: data.country), const TextSpan(text: ", "), TextSpan(text: data.city), const TextSpan(text: " "), TextSpan(text: data.time)]
+                              ),
+                            ),
+                            RichText(
+                              text: TextSpan(
+                                style: const TextStyle(fontSize: 24, fontFamily: "kanit", fontWeight: FontWeight.w900),
+                                children: [TextSpan(text: data.name), const TextSpan(text: ", "), TextSpan(text: data.age)]
+                              ),
+                            ),
+                            RichText(
+                              text: const TextSpan(
+                                style: TextStyle(fontSize: 14, fontFamily: "kanit", fontWeight: FontWeight.w700),
+                                children: [TextSpan(text: "TH"), WidgetSpan(child: Icon(Icons.sync_alt, size: 20, color: textLight,)), TextSpan(text: "KR"),]
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(right: 20),
+                        child: InkWell(
+                          onTap: () { print("หัวใจ"); },
+                          child: Expanded(child: SvgPicture.asset("assets/icons/heart_button.svg"))
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
