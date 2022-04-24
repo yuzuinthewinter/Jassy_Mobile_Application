1e4b4e3ac0c8efbcec2fa7b65cea77b861716cae
dnazzx <zzan-official@gmail.com>
2022-04-24 22:06:25 +0700

    update list user

--- frontend/lib/screens/chat/component/list_user.dart
+++ frontend/lib/screens/chat/component/list_user.dart
@@ -41,90 +41,81 @@ class _ListUserBody extends State<ListUser> {
         builder: (context, snapshot) {
           var user = snapshot.data!.docs;
           final userData = user.map((doc) => {doc.data()}).toList();
-          // if (user[0]['chats'].length > 0) {
-
-          // ,Text('this');
-          // var length = user[0]['chats'].length;
-          // for (int i in length) {
-          //   print(i);
-          //   // var getroom = chatRooms.doc(user[0]['chats'][i]);
-          //   // print(getroom.toString());
-          // }
-          // }
+          //count user online
           if (user.isEmpty) {
             return const Center(
               child: Text('Let\'s talk with the stranger!'),
             );
           } else {
-            
-            return Text(user[0]['chats'].toString());
-            // return ListView.builder(
-            //     scrollDirection: Axis.horizontal,
-            //     itemCount: userData.length,
-            //     itemBuilder: (context, int index) => SizedBox(
-            //           child: InkWell(
-            //             // onTap: () {
-            //             //   // createChatRoom();
-            //             //   Navigator.push(context,
-            //             //       CupertinoPageRoute(builder: (context) {
-            //             //     // NOTE: click each card to go to chat room
-            //             //     return ChatRoom(
-            //             //       user: user[index],
-            //             //     );
-            //             //   }));
-            //             // },
-            //             child: Padding(
-            //               padding: const EdgeInsets.all(5.0),
-            //               child: Column(
-            //                 children: [
-            //                   // getChat(user[index]['chats']),
-            //                   Text(chatRooms.doc(user[index]['chats']).toString()),
-            //                   // Stack(
-            //                   //   children: [
-            //                   // CircleAvatar(
-            //                   //   backgroundImage: !user[index]['profilePic']
-            //                   //           .isEmpty
-            //                   //       ? NetworkImage(
-            //                   //           user[index]['profilePic'][0])
-            //                   //       : const AssetImage(
-            //                   //               "assets/images/header_img1.png")
-            //                   //           as ImageProvider,
-            //                   //   radius: 33,
-            //                   // ),
-            //                   // chats[index]['isActive'] == true
-            //                   //     ? Positioned(
-            //                   //         right: 3,
-            //                   //         bottom: 3,
-            //                   //         child: Container(
-            //                   //           height: 16,
-            //                   //           width: 16,
-            //                   //           decoration: BoxDecoration(
-            //                   //               color: onlineColor,
-            //                   //               shape: BoxShape.circle,
-            //                   //               border: Border.all(
-            //                   //                   color: Theme.of(context)
-            //                   //                       .scaffoldBackgroundColor)),
-            //                   //         ),
-            //                   //       )
-            //                   //     : const Text(''),
-            //                   //   ],
-            //                   // ),
-            //                   // SizedBox(
-            //                   //   width: 70.0,
-            //                   //   child: Text(
-            //                   //     user[index]['name']['firstname'] +
-            //                   //         ' ' +
-            //                   //         user[index]['name']['lastname'],
-            //                   //     overflow: TextOverflow.ellipsis,
-            //                   //     maxLines: 2,
-            //                   //     textAlign: TextAlign.center,
-            //                   //   ),
-            //                   // ),
-            //                 ],
-            //               ),
-            //             ),
-            //           ),
-            //         ));
+            // return Text(user[0]['chats'].toString());
+            return ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: userData.length,
+                itemBuilder: (context, int index) => SizedBox(
+                      child: InkWell(
+                        onTap: () {
+                          // createChatRoom();
+                          Navigator.push(context,
+                              CupertinoPageRoute(builder: (context) {
+                            // NOTE: click each card to go to chat room
+                            return ChatRoom(
+                              user: user[index],
+                            );
+                          }));
+                        },
+                        child: Padding(
+                          padding: const EdgeInsets.all(5.0),
+                          child: Column(
+                            children: [
+                              // getChat(user[index]['chats']),
+                              // Text(chatRooms.doc(user[index]['chats']).toString()),
+                              Stack(
+                                children: [
+                                  CircleAvatar(
+                                    backgroundImage: !user[index]['profilePic']
+                                            .isEmpty
+                                        ? NetworkImage(
+                                            user[index]['profilePic'][0])
+                                        : const AssetImage(
+                                                "assets/images/header_img1.png")
+                                            as ImageProvider,
+                                    radius: 33,
+                                  ),
+                                  // chats[index]['isActive'] == true
+                                  // ?
+                                  Positioned(
+                                    right: 3,
+                                    bottom: 3,
+                                    child: Container(
+                                      height: 16,
+                                      width: 16,
+                                      decoration: BoxDecoration(
+                                          color: onlineColor,
+                                          shape: BoxShape.circle,
+                                          border: Border.all(
+                                              color: Theme.of(context)
+                                                  .scaffoldBackgroundColor)),
+                                    ),
+                                  )
+                                  // : const Text(''),
+                                ],
+                              ),
+                              SizedBox(
+                                width: 70.0,
+                                child: Text(
+                                  user[index]['name']['firstname'] +
+                                      ' ' +
+                                      user[index]['name']['lastname'],
+                                  overflow: TextOverflow.ellipsis,
+                                  maxLines: 2,
+                                  textAlign: TextAlign.center,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ));
           }
         },
       ),
