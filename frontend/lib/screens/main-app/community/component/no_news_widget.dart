5397acbd92529d41aedc2e41bea6f0df7bb3afdb
nutcpl <59905941+nutcpl@users.noreply.github.com>
2022-05-05 13:20:53 +0700

    no news widget

---
+++ frontend/lib/screens/main-app/community/component/no_news_widget.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/theme/index.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class NoNewsWidget extends StatelessWidget {
+
+  final String headText, descText;
+  
+  const NoNewsWidget({
+    Key? key,
+    required this.size, this.headText = '', this.descText = '',
+  }) : super(key: key);
+
+  final Size size;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SizedBox(height: size.height * 0.03,),
+        SvgPicture.asset("assets/images/no_news_image.svg", width: size.width * 0.55,),
+        SizedBox(height: size.height * 0.02,),
+        Text(headText, style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),),
+        SizedBox(height: size.height * 0.01,),
+        Text(descText, style: TextStyle(fontSize: 16, color: greyDark),),
+      ],
+    );
+  }
+}
