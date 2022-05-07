b81ebd863d011c6cac4e80094c6209711b917ae3
nutcpl <59905941+nutcpl@users.noreply.github.com>
2022-05-08 01:19:25 +0700

    add search group

--- frontend/lib/screens/main-app/community/community_search/community_search.dart
+++ frontend/lib/screens/main-app/community/community_search/community_search.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/component/back_close_appbar.dart';
+import 'package:flutter_application_1/screens/main-app/community/community_search/component/community_search_body.dart';
 
 class CommunitySearch extends StatelessWidget {
   const CommunitySearch({ Key? key }) : super(key: key);
@@ -7,7 +8,9 @@ class CommunitySearch extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      extendBodyBehindAppBar: true,
       appBar: BackAndCloseAppBar(text: "ค้นหากลุ่ม",),
+      body: CommunitySearchBody(),
     );
   }
 }
---
+++ frontend/lib/screens/main-app/community/community_search/component/group_for_search_widget.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/models/community.dart';
+import 'package:flutter_application_1/theme/index.dart';
+
+Widget groupForSearchWidget (GroupActivity group, context) {
+  var size = MediaQuery.of(context).size;
+  return Padding(
+    padding: EdgeInsets.symmetric(horizontal: size.width * 0.05),
+    child: Container(
+      padding: EdgeInsets.symmetric(horizontal: size.width * 0.05),
+      width: double.infinity,
+      height: size.height * 0.15,
+      decoration: BoxDecoration(
+        color: textLight,
+        borderRadius: BorderRadius.all(Radius.circular(20)),
+        boxShadow: [
+          BoxShadow(
+            color: grey.withOpacity(0.3),
+            blurRadius: 3,
+            offset: Offset(0, 5),
+          )
+        ]
+      ),
+      child: Row(
+        children: [
+          // Group Picture
+          ClipRRect(
+            borderRadius: BorderRadius.circular(20.0),
+            child: Container(
+              width: size.width * 0.21,
+              height: size.height * 0.1,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage(group.groupName.image),
+                  fit: BoxFit.cover
+                ),
+              ),
+            ),
+          ),
+          // Group Name + member count
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: size.width * 0.05),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(group.groupName.name, style: TextStyle(fontSize: 20, fontWeight: FontWeight.w700),),
+                  SizedBox(height: size.height * 0.01,),
+                  RichText(
+                      text: TextSpan(
+                      style: const TextStyle(fontSize: 14, color: greyDark, fontFamily: 'kanit'),
+                      children: [
+                        const WidgetSpan(child: Icon(Icons.person_rounded, color: grey,)),
+                        WidgetSpan(child: SizedBox(width: size.width * 0.01,),),
+                        TextSpan(text: group.groupName.member.toString(), style: TextStyle(fontSize: 16),)
+                      ]
+                    )
+                  )
+                ],
+              ),
+            ),
+          ),
+          // add icon
+          InkWell(
+            onTap: () {},
+            child: Icon(Icons.add_circle_rounded, color: primaryColor, size: size.width * 0.1,)
+          )
+        ],
+      ),
+    ),
+  );
+}
