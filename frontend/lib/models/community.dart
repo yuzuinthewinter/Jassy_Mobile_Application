304107bcf511004ce9fbc03d0902ee81765a9d55
nutcpl <59905941+nutcpl@users.noreply.github.com>
2022-05-07 16:04:10 +0700

    community model

--- frontend/lib/models/community.dart
+++ frontend/lib/models/community.dart
@@ -2,53 +2,90 @@ import 'package:flutter/material.dart';
 
 class Community {
   final String name, image;
-  final int member;
+  final int id, member;
 
   Community({
+    required this.id,
     this.name = '', 
     this.image = '',
     this.member = 0,
   });
 }
 
-List<Community> communityLists = [
-  Community(
-    name: "Thai",
-    image: "assets/images/user1.jpg",
-    member: 123
+final Community thai = Community(
+  id: 1,
+  name: "Thai",
+  image: "assets/images/user1.jpg",
+  member: 123
+);
+final Community korean = Community(
+  id: 2,
+  name: "Korean",
+  image: "assets/images/user2.jpg",
+  member: 1200
+);
+final Community english = Community(
+  id: 3,
+  name: "English",
+  image: "assets/images/user3.jpg",
+  member: 4652
+);
+final Community indonesia = Community(
+  id: 1,
+  name: "Indonesiaaaaaaaaaaaaaa",
+  image: "assets/images/user4.jpg",
+  member: 321
+);
+
+class GroupActivity {
+  Community groupName;
+  // final String writer, news, image;
+  // final List comment;
+  // final int like, member;
+  // final DateTime date;
+
+  GroupActivity({
+    required this.groupName,
+    // this.writer = '',
+    // this.news = '',
+    // this.comment = const [],
+    // this.image = '',
+    // this.like = 0,
+    // this.member = 0,
+    // required this.date,
+  });
+}
+
+final List<GroupActivity> groupLists = [
+  GroupActivity(
+    groupName: thai,
   ),
-  Community(
-    name: "Korean",
-    image: "assets/images/user2.jpg",
-    member: 1200
+  GroupActivity(
+    groupName: korean,
   ),
-  Community(
-    name: "English",
-    image: "assets/images/user3.jpg",
-    member: 4652
+  GroupActivity(
+    groupName: english,
   ),
-  Community(
-    name: "Indonesiaaaaaaaaaaaaaa",
-    image: "assets/images/user4.jpg",
-    member: 321
+  GroupActivity(
+    groupName: indonesia,
   ),
 ];
 
 class News {
-  final String writer, news, comment, image, groupName;
-  // final List<String> comm;
+  Community groupName;
+  final String writer, news;
+  final List comment, image;
   final int like;
   // final DateTime date;
 
   News({
+    required this.groupName,
     this.writer = '',
     this.news = '',
-    this.comment = '',
-    this.image = '',
+    this.comment = const [],
+    this.image = const [],
     this.like = 0,
-    this.groupName = '',
     // required this.date,
-    // this.comm = [],
   });
 }
 
@@ -56,49 +93,49 @@ List<News> newsLists = [
   News(
     writer: "",
     news: "",
-    comment: "",
-    image: "",
+    comment: [],
+    image: [],
     like: 100,
-    groupName: "",
+    groupName: thai,
   ),
   News(
     writer: "",
     news: "",
-    comment: "",
-    image: "",
+    comment: [],
+    image: [],
     like: 100,
-    groupName: "",
+    groupName: korean,
   ),
   News(
     writer: "",
     news: "",
-    comment: "",
-    image: "",
+    comment: [],
+    image: [],
     like: 100,
-    groupName: "",
+    groupName: english,
   ),
   News(
     writer: "",
     news: "",
-    comment: "",
-    image: "",
+    comment: [],
+    image: [],
     like: 100,
-    groupName: "",
+    groupName: indonesia,
   ),
   News(
     writer: "",
     news: "",
-    comment: "",
-    image: "",
+    comment: [],
+    image: [],
     like: 100,
-    groupName: "",
+    groupName: korean,
   ),
   News(
     writer: "",
     news: "",
-    comment: "",
-    image: "",
+    comment: [],
+    image: [],
     like: 100,
-    groupName: "",
+    groupName: english,
   ),
 ];
