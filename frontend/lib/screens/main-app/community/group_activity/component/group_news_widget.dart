bf54bf50e2305164a670fddf21f12c2e675327c1
nutcpl <59905941+nutcpl@users.noreply.github.com>
2022-05-09 19:43:06 +0700

    Fix news post UI (#67)
    * resize icon
    
    * post detail page
    
    * add comment tree package
    
    * add comment tree
    
    * add navigator
    
    * add data
    
    * add reply onTab
    
    * fix all news widget ui
    
    * add more icon

--- frontend/lib/screens/main-app/community/group_activity/component/group_news_widget.dart
+++ frontend/lib/screens/main-app/community/group_activity/component/group_news_widget.dart
@@ -8,55 +8,79 @@ import 'package:flutter_svg/flutter_svg.dart';
   Widget groupNewsCard (News data, context) {
     var size = MediaQuery.of(context).size;
     return Container(
-          padding: EdgeInsets.symmetric(horizontal: size.width * 0.05, vertical: size.height * 0.02),
-          width: size.width * 0.25,
-          height: size.height * 0.25,
-          decoration: BoxDecoration(
-            color: textLight,
-            border: Border(bottom: BorderSide(width: size.width * 0.01, color: primaryLightest))
-          ),
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              CircleAvatar(
-                backgroundImage: const AssetImage("assets/images/user3.jpg"),
-                radius: size.width * 0.08,
-              ),
-              Expanded(
-                child: Padding(
-                  padding: EdgeInsets.symmetric(horizontal: size.width * 0.03,),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      // post by
-                      RichText(
-                        text: const TextSpan(
-                          style: TextStyle(color: greyDark, fontSize: 14, fontFamily: 'kanit'),
-                          children: [
-                            TextSpan(text: "เขียนโดย "),
-                            TextSpan(text: "Ammie "),
-                            TextSpan(text: "เมื่อ "),
-                            TextSpan(text: "10:24 AM "),
-                          ]                      
+      padding: EdgeInsets.symmetric(vertical: size.height * 0.02),
+        decoration: BoxDecoration(
+          color: textLight,
+          border: Border(bottom: BorderSide(width: size.width * 0.01, color: primaryLightest))
+        ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        // mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: size.width * 0.05),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                CircleAvatar(
+                  backgroundImage: const AssetImage("assets/images/user3.jpg"),
+                  radius: size.width * 0.08,
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: size.width * 0.03,),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        SizedBox(height: size.height * 0.025,),
+                        // post by
+                        RichText(
+                          text: const TextSpan(
+                            style: TextStyle(color: greyDark, fontSize: 14, fontFamily: 'kanit'),
+                            children: [
+                              TextSpan(text: "เขียนโดย "),
+                              TextSpan(text: "Ammie "),
+                              TextSpan(text: "เมื่อ "),
+                              TextSpan(text: "10:24 AM "),
+                            ]                      
+                          ),
                         ),
-                      ),
-                      // caption
-                      SizedBox(height: size.height * 0.005),
-                      Expanded(child: Text(data.news, style: TextStyle(fontSize: 18), maxLines: 3,)),
-                      // like and comment
-                      Row(
-                        children: [
-                          const LikeButtonWidget(),
-                          SizedBox(width: size.width * 0.05),
-                          SvgPicture.asset("assets/icons/comment_icon.svg", width: size.width * 0.07,)
-                        ],
-                      ),
-                    ],
-                  ),
-                )
-              ),
-              Icon(Icons.more_horiz, color: primaryColor, size: size.width * 0.08,)
-            ],
+                      ],
+                    ),
+                  )
+                ),
+                Icon(Icons.more_horiz, color: primaryColor, size: size.width * 0.08,)
+              ],
+            ),
           ),
-        );
+          SizedBox(height: size.height * 0.02,),
+          // post text
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: size.width * 0.1),
+            child: Container(
+              constraints: const BoxConstraints(maxHeight: double.infinity),
+              child: Text(data.news, maxLines: null, style: TextStyle(fontSize: 18),),
+            ),
+          ),
+          SizedBox(height: size.height * 0.03,),
+          // like and comment icon
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: size.width * 0.1),
+            child: Row(
+              children: [
+                const LikeButtonWidget(),
+                SizedBox(width: size.width * 0.05),
+                InkWell(
+                  onTap: () {
+                    // FocusScope.of(context).requestFocus(myFocusNode);
+                  },
+                  child: SvgPicture.asset("assets/icons/comment_icon.svg", width: size.width * 0.07,)
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
