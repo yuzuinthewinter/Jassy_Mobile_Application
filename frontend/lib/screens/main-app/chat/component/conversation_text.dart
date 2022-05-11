e9744947be6b51e21f08edbed671997a55347935
nutcpl <59905941+nutcpl@users.noreply.github.com>
2022-05-11 13:23:20 +0700

    Chat tooltips (#73)
    * add item model
    
    * add package
    
    * add longPress pop-up menu
    
    * add icons
    
    * remove container

--- frontend/lib/screens/main-app/chat/component/conversation_text.dart
+++ frontend/lib/screens/main-app/chat/component/conversation_text.dart
@@ -1,8 +1,11 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/models/item.dart';
 import 'package:flutter_application_1/theme/index.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:intl/intl.dart';
+import 'package:custom_pop_up_menu/custom_pop_up_menu.dart';
 
 class ConversationText extends StatefulWidget {
   final chatid;
@@ -131,15 +134,19 @@ class _BodyState extends State<ConversationText> {
                       ]
                     ],
                   ),
-                  Container(
-                      constraints: BoxConstraints(
-                          maxWidth: MediaQuery.of(context).size.width * 0.6),
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 20, vertical: 10),
-                      decoration: BoxDecoration(
-                          color: isCurrentUser ? primaryLighter : textLight,
-                          borderRadius: BorderRadius.circular(20)),
-                      child: Text(currentMessage['message'])
+                  CustomPopupMenu(
+                    menuBuilder: _buildLongPressMenu, 
+                    pressType: PressType.longPress,
+                    child: Container(
+                        constraints: BoxConstraints(
+                            maxWidth: MediaQuery.of(context).size.width * 0.6),
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 20, vertical: 10),
+                        decoration: BoxDecoration(
+                            color: isCurrentUser ? primaryLighter : textLight,
+                            borderRadius: BorderRadius.circular(20)),
+                        child: Text(currentMessage['message'])
+                    ),
                   ),
                   SizedBox(width: size.height * 0.01,),
                   if (!isCurrentUser) ...[
@@ -150,28 +157,73 @@ class _BodyState extends State<ConversationText> {
                   ],
                 ],
               ),
-              // Row(
-              //   mainAxisAlignment: isCurrentUser
-              //       ? MainAxisAlignment.end
-              //       : MainAxisAlignment.start,
-              //   children: [
-              //     if (!isCurrentUser)
-              //       SizedBox(
-              //         width: size.height * 0.06,
-              //       ),
-              //     Text(
-              //       getDate(currentMessage['time']).toString(),
-              //       style: const TextStyle(color: grey, fontSize: 12),
-              //     ),
-              //     SizedBox(
-              //       width: size.height * 0.01,
-              //     ),
-              //   ],
-              // )
             ],
           ),
         );
       },
+    );
+  }
+
+ Widget _buildLongPressMenu() {
+    List<ItemModel> menuItems = [
+      ItemModel(id: 1, text: "ตอบกลับ", icon: "assets/icons/reply_icon.svg"),
+      ItemModel(id: 2, text: "คัดลอก", icon: "assets/icons/copy_icon.svg"),
+      ItemModel(id: 3, text: "แปล", icon: "assets/icons/translate_icon.svg"),
+      ItemModel(id: 4, text: "ชอบ", icon: "assets/icons/favorite_icon.svg"),
+    ];
+    var item1 = menuItems[0].id;
+    var item2 = menuItems[1].id;
+    var item3 = menuItems[2].id;
+    var item4 = menuItems[3].id;
+
+    Size size = MediaQuery.of(context).size;
+    return Container(
+      width: size.width * 0.6,
+      height: size.height * 0.08,
+      decoration: const BoxDecoration(
+        color: primaryDark,
+        borderRadius: BorderRadius.all(Radius.circular(10)),
+      ),
+      child: GridView.count(
+        padding: const EdgeInsets.symmetric(vertical: 0),
+          crossAxisCount: 4,     
+          children: menuItems
+            .map((item) => 
+            Container(
+              decoration: const BoxDecoration(
+                border: Border(right: BorderSide(color: textLight, width: 0.1))
+              ),
+              child: InkWell(
+                onTap: () {
+                  // Todo: add onTab here
+                  if(item.id == item1) {
+                    return print("reply");
+                  } else if (item.id == item2) {
+                    return print("copy");
+                  } else if (item.id == item3) {
+                    return print("translate");
+                  } else {
+                    print("favorite");
+                  }
+                },
+                child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        SvgPicture.asset(item.icon),
+                        Container(
+                          margin: EdgeInsets.only(top: 2),
+                          child: Text(
+                            item.text,
+                            style: TextStyle(color: Colors.white, fontSize: 12),
+                          ),
+                        ),
+                      ],
+                    ),
+              ),
+            ))
+            .toList(),
+        ),
     );
   }
 }
