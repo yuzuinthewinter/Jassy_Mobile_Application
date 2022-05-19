1f8616a19bc10ac4ae2a8cc8f9e14924c0acf62e
nutcpl <59905941+nutcpl@users.noreply.github.com>
2022-05-19 21:48:52 +0700

    commit

--- frontend/lib/screens/main-app/profile/component/profile_body.dart
+++ frontend/lib/screens/main-app/profile/component/profile_body.dart
@@ -10,6 +10,7 @@ import 'package:flutter_application_1/screens/main-app/profile/profile_setting/p
 import 'package:flutter_application_1/screens/main-app/profile/component/profile_menu_widget.dart';
 import 'package:flutter_application_1/screens/main-app/profile/component/profile_picture_widget.dart';
 import 'package:flutter_application_1/screens/main-app/profile/saved_post/saved_post.dart';
+import 'package:flutter_application_1/screens/pre-app/register_info/picture_upload.dart';
 import 'package:flutter_application_1/theme/index.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
@@ -143,7 +144,12 @@ class _ProfileScreenBody extends State<ProfileScreenBody> {
                     size: size,
                     icon: SvgPicture.asset("assets/icons/help_center_icon.svg"),
                     text: "ProfileHelp".tr,
-                    onTab: () {},
+                    onTab: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => const PictureUpload()),
+                      );
+                    },
                 ),     
                 ProfileMenu(
                   size: size,
