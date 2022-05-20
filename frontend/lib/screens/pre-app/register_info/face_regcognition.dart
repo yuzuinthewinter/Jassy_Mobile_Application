08896ce0c277eb5036709bbab8722f05df661087
nutcpl <59905941+nutcpl@users.noreply.github.com>
2022-05-20 23:50:18 +0700

    commit

--- frontend/lib/screens/pre-app/register_info/face_regcognition.dart
+++ frontend/lib/screens/pre-app/register_info/face_regcognition.dart
@@ -1,4 +1,8 @@
+import 'dart:ffi';
+
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+import 'package:google_ml_kit/google_ml_kit.dart';
 
 class FaceRecognition extends StatefulWidget {
   const FaceRecognition({Key? key}) : super(key: key);
@@ -8,6 +12,53 @@ class FaceRecognition extends StatefulWidget {
 }
 
 class _FaceRecognitionState extends State<FaceRecognition> {
+  CameraController? _cameraController;
+  CameraController? get cameraController => this._cameraController;
+
+  InputImageRotation? _cameraRotation;
+  InputImageRotation? get cameraRotation => this._cameraRotation;
+
+  Future _setupCameraController({
+    required CameraDescription description,
+  }) async {
+    this._cameraController = CameraController(
+      description,
+      ResolutionPreset.high,
+      enableAudio: false,
+    );
+    await _cameraController?.initialize();
+  }
+  
+  Future<void> initialize() async {
+    if (_cameraController != null) return;
+    CameraDescription description = await _getCameraDescription();
+    await _setupCameraController(description: description);
+    this._cameraRotation = rotationIntToImageRotation(
+      description.sensorOrientation,
+    );
+  }
+
+  Future<CameraDescription> _getCameraDescription() async {
+    List<CameraDescription> cameras = await availableCameras();
+    return cameras.firstWhere((CameraDescription camera) =>
+        camera.lensDirection == CameraLensDirection.front);
+  }
+
+  InputImageRotation rotationIntToImageRotation(int rotation) {
+    switch (rotation) {
+      case 90:
+        return InputImageRotation.rotation90deg;
+      case 180:
+        return InputImageRotation.rotation180deg;
+      case 270:
+        return InputImageRotation.rotation270deg;
+      default:
+        return InputImageRotation.rotation0deg;
+    }
+  }
+
+
+
   @override
   Widget build(BuildContext context) {
     return Container();
