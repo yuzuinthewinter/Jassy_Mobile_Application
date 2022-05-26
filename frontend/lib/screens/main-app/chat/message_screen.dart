d2bee5c38c3846a89f2cf56f1c7bc0d698179e93
nutcpl <59905941+nutcpl@users.noreply.github.com>
2022-05-26 23:45:39 +0700

    chat app bar color (#114)

--- frontend/lib/screens/main-app/chat/message_screen.dart
+++ frontend/lib/screens/main-app/chat/message_screen.dart
@@ -132,6 +132,7 @@ class _ChatRoomState extends State<ChatRoom> with WidgetsBindingObserver {
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
     return Scaffold(
+      extendBodyBehindAppBar: true,
       appBar: AppBar(
         title: Column(
           children: [
