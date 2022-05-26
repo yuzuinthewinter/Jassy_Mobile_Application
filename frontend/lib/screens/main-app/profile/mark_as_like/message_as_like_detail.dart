3691e5ccc9fd9b62aff6de6987a6c7907c6cf35c
nutcpl <59905941+nutcpl@users.noreply.github.com>
2022-05-27 02:19:19 +0700

    mark as like datail

--- frontend/lib/screens/main-app/profile/mark_as_like/message_as_like_detail.dart
+++ frontend/lib/screens/main-app/profile/mark_as_like/message_as_like_detail.dart
@@ -2,9 +2,11 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/component/appbar/back_and_delete_appbar.dart';
+import 'package:flutter_application_1/component/button/round_button.dart';
 import 'package:flutter_application_1/component/popup_page/delete_popup.dart';
 import 'package:flutter_application_1/models/item.dart';
 import 'package:flutter_application_1/theme/index.dart';
+import 'package:get/utils.dart';
 
 class MessageAsLikeDetail extends StatefulWidget {
   final int color;
@@ -57,74 +59,65 @@ class _MessageAsLikeDetailState extends State<MessageAsLikeDetail> {
                   });
                 });
           }),
-      body: Column(
-        children: [
-          Expanded(
-            child: Container(
-              padding: EdgeInsets.symmetric(horizontal: size.width * 0.05),
-              width: double.infinity,
-              height: size.height * 0.3,
-              color: colors[widget.color],
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    widget.message['message'],
-                    style: TextStyle(fontSize: 16),
-                  )
-                ],
+      body: Container(
+        color: isShow ? colors[widget.color] : widget.color == 2 ? colors[0] : colors[widget.color + 1],
+        child: Column(
+          children: [
+            Expanded(
+              child: Container(
+                padding: EdgeInsets.symmetric(horizontal: size.width * 0.05),
+                width: double.infinity,
+                height: size.height * 0.3,
+                color: colors[widget.color],
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      widget.message['message'],
+                      style: TextStyle(fontSize: 16),
+                    )
+                  ],
+                ),
               ),
             ),
-          ),
-          Expanded(
-            child: Container(
-              padding: EdgeInsets.symmetric(
-                  horizontal: size.width * 0.05, vertical: size.height * 0.05),
-              width: double.infinity,
-              height: size.height * 0.42,
-              color: widget.color == 2 ? colors[0] : colors[widget.color + 1],
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Stack(
-                    children: [
-                      Align(
-                      alignment: Alignment.topRight,
-                      child: IconButton(
-                            onPressed: () {
-                              setState(() {
-                                isShow = !isShow;
-                              });
-                            },
-                            icon: isShow
-                                  ? Icon(Icons.visibility_off_rounded,)
-                                  : Icon(Icons.visibility_rounded,),
-                              color: primaryDarker,
-                            ),
+            !isShow ? Expanded(
+              child: Container(
+                padding: EdgeInsets.symmetric(
+                    horizontal: size.width * 0.05, vertical: size.height * 0.05),
+                width: double.infinity,
+                height: size.height * 0.42,
+                color: widget.color == 2 ? colors[0] : colors[widget.color + 1],
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Align(
+                      alignment: Alignment.center,
+                      child: Text(
+                        widget.translate.targetLanguage.toString().toUpperCase(),
+                          style: TextStyle(fontSize: 20),
                       ),
-                      Align(
-                        alignment: Alignment.center,
-                        child: Padding(
-                          padding: EdgeInsets.only(top: size.width * 0.02),
-                          child: Text(
-                            widget.translate.targetLanguage.toString().toUpperCase(),
-                            style: TextStyle(fontSize: 20),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                  !isShow
-                      ? const SizedBox.shrink()
-                      : Text(
-                          widget.translate.toString(),
-                          style: TextStyle(fontSize: 16),
-                        )
-                ],
+                    ),
+                    SizedBox(height: size.height * 0.01,),
+                    Text(
+                      widget.translate.toString(),
+                      style: TextStyle(fontSize: 16),
+                    )
+                  ],
+                ),
               ),
+            ) : const SizedBox.shrink(),
+            RoundButton(
+              text: isShow ? "Translate".tr : "Cancel".tr, 
+              minimumSize: Size(size.width * 0.8, size.height * 0.05), 
+              press: () {
+                setState(() {
+                  isShow = !isShow;
+                });
+              }
             ),
-          ),
-        ],
+            SizedBox(height: size.height * 0.02,)
+          ],
+        ),
       ),
     );
   }
